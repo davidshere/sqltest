ee159efdad413cbdf3044fc136e4f651fcb4e001
davidshere <sheredlk@gmail.com>
2021-07-13 13:29:44 -0700

    create and destroy test databases

--- build.sbt
+++ build.sbt
@@ -6,10 +6,17 @@ lazy val hello = (project in file("."))
     name := "Hello",
 
     libraryDependencies ++= Seq(
+      // Test dependencies
       "org.scalatest"   %% "scalatest"          % "3.2.7" % Test,
+
+      // Connecting to a database
       "org.scalikejdbc" %% "scalikejdbc"        % "3.5.+",
       "org.postgresql"  %  "postgresql"         % "42.2.23",
-      "ch.qos.logback"  %  "logback-classic"    % "1.2.+"
+      "ch.qos.logback"  %  "logback-classic"    % "1.2.+",
+
+      // Reading CSV and YAML
+      "com.fasterxml.jackson.dataformat" % "jackson-dataformat-csv" % "2.12.4",
+      "com.fasterxml.jackson.dataformat" % "jackson-dataformat-yaml" % "2.12.4",
     ),
 
   )	 
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,11 +1,26 @@
 package example
 
+import scala.io.Source
+import scala.util.Random
+
 import scalikejdbc._
 
+import ch.qos.logback.classic.{Level,Logger}
+import org.slf4j.LoggerFactory
+
 object Hello extends App {
+  // Set logging level
+  LoggerFactory
+    .getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME)
+    .asInstanceOf[Logger]
+    .setLevel(Level.INFO)
+
+  // Connect to DB
   Class.forName("org.postgresql.Driver")
   ConnectionPool.singleton("jdbc:postgresql://0.0.0.0:5432/postgres", "postgres", "mysecretpassword")
 
+  val testDBName = s"test_database_${Random.alphanumeric.take(6).mkString}"
+  val createdTables: Seq[String] = Seq() 
 
   def runTestQuery: Int = {
     val result: Option[Int] = DB readOnly { implicit session =>
@@ -14,5 +29,33 @@ object Hello extends App {
     result.getOrElse(throw new RuntimeException("no results"))
   }
 
-  println(runTestQuery)
+  def createTestDB = DB autoCommit { implicit session =>
+    val dbName = SQLSyntax.createUnsafely(testDBName)
+    sql"CREATE DATABASE ${dbName}".execute.apply()
+  }
+
+  def destroyTestDB = DB autoCommit { implicit session =>
+    val dbName = SQLSyntax.createUnsafely(testDBName)
+    println(dbName)
+    sql"DROP DATABASE ${dbName}".execute.apply()
+  }
+
+ /*
+  *
+  */
+  def setUp = {
+    createTestDB
+  }
+
+  def tearDown = {
+    destroyTestDB
+  }
+
+  def main = {
+    setUp
+    println(runTestQuery)
+    tearDown
+  }
+
+  main
 }
