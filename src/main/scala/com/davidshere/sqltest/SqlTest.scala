714edda292d16bcdee6e4e9a0cf62591f9e5a9da
davidshere <sheredlk@gmail.com>
2021-07-17 21:13:39 -0400

    update tests

--- src/main/scala/com/davidshere/sqltest/SqlTest.scala
+++ src/main/scala/com/davidshere/sqltest/SqlTest.scala
@@ -1,5 +1,95 @@
 package com.davidshere.sqltest
 
-object SqlTest {
+import java.sql.{Connection, DriverManager, ResultSet, Statement}
 
+import scala.io.Source
+import scala.util.Random
+import scala.jdk.CollectionConverters._
+import net.sf.jsqlparser.parser.CCJSqlParserUtil
+import net.sf.jsqlparser.statement.create.table.CreateTable
+import net.sf.jsqlparser.statement.{Statement => ParserStatement}
+import net.sf.jsqlparser.statement.select.Select
+import net.sf.jsqlparser.util.TablesNamesFinder
+
+object SqlTest extends App {
+  /*
+  Class.forName("org.postgresql.Driver")
+  def getDBConn(dbName: String): Connection = DriverManager.getConnection(
+    s"jdbc:postgresql://0.0.0.0:5432/${dbName}",
+    "postgres",
+    "mysecretpassword"
+  )
+
+
+  val conn: Connection = getDBConn("postgres")
+  val stmt: Statement = conn.createStatement()
+  */
+  val testDBName = s"test_database_${Random.alphanumeric.take(6).mkString}"
+  val createdTables: Seq[String] = Seq()
+  /*
+  def runTestQuery: Int = {
+    val query: String = "select 1 as v"
+    val result: ResultSet = stmt.executeQuery(query)
+    result.next()
+    result.getInt("v")
+  }
+  */
+  def getTableNameFromSimpleQuery(query: String): List[String] = {
+    val statement: ParserStatement = CCJSqlParserUtil.parse(query)
+    val selectStmt: Select = statement.asInstanceOf[Select]
+    val nameFinder: TablesNamesFinder = new TablesNamesFinder()
+
+    nameFinder.getTableList(selectStmt).asScala.toList
+  }
+
+  def getTableNameFromCreateTable(create: String): List[String] = {
+    val statement: ParserStatement = CCJSqlParserUtil.parse(create)
+    val createStmt: CreateTable = statement.asInstanceOf[CreateTable]
+    val nameFinder: TablesNamesFinder = new TablesNamesFinder()
+
+    nameFinder.getTableList(createStmt).asScala.toList
+  }
+
+  def getTestSchema(filename: String): String =
+    Source.fromResource(filename).mkString
+
+  def getCsvData(filename: String): String = Source.fromResource(filename).mkString
+
+  /*
+  def createTestDB = {
+    val sql = s"CREATE DATABASE ${testDBName}"
+    stmt.executeUpdate(sql)
+  }
+
+  def destroyTestDB = {
+    val sql = s"DROP DATABASE ${testDBName}"
+    stmt.executeUpdate(sql)
+  }
+
+ /*
+  *
+  */
+  def setUp = {
+    createTestDB
+  }
+
+  def tearDown = {
+    destroyTestDB
+    conn.close()
+  }
+  */
+
+  def main = {
+    // setUp
+    val schema: String = getTestSchema("table2.sql")
+    val creates = schema.strip.split(";").map(_.strip)
+    val names = creates.map(getTableNameFromCreateTable)
+    println(names.length)
+    println(names.foreach(_.mkString))
+    println(createdTables)
+    // schema.split(';').foreach(stmt.executeUpdate(_))
+    // tearDown
+  }
+
+  main
 }
--- src/test/scala/com/davidshere/sqltest/SqlTestSpec.scala
+++ src/test/scala/com/davidshere/sqltest/SqlTestSpec.scala
@@ -1,7 +1,28 @@
+package com.davidshere.sqltest
+
 import org.scalatest.funsuite._
 
 class SqlTestSpec extends AnyFunSuite {
-  test("Hello should start with H") {
-    assert("hello".startsWith("h"))
+  test("Parser should parse table names from SELECT") {
+    val queryBasic = "SELECT * FROM table1"
+    assert(SqlTest.getTableNameFromSimpleQuery(queryBasic) == List("table1"))
+
+    val queryWhere = "SELECT * FROM table1 WHERE starfish='green'"
+    assert(SqlTest.getTableNameFromSimpleQuery(queryWhere) == List("table1"))
+
+    val queryJoin = "SELECT * FROM table1 JOIN table2 USING(starfish)"
+    assert(SqlTest.getTableNameFromSimpleQuery(queryJoin) == List("table1", "table2"))
+
+    val queryAlias = "SELECT * FROM table1 JOIN (select * from table2) as fish using (starfish)"
+    assert(SqlTest.getTableNameFromSimpleQuery(queryAlias) == List("table1", "table2"))
+
+    val queryWith = "WITH tmp AS (SELECT * FROM table1) SELECT * from tmp;"
+    assert(SqlTest.getTableNameFromSimpleQuery(queryWith) == List("table1"))
+  }
+
+  test("Parse should parse table names from CREATE") {
+    val stmt = "CREATE TABLE table1 (val1 INT PRIMARY KEY)"
+    assert(SqlTest.getTableNameFromCreateTable(stmt) == List("table1"))
+
   }
 }
